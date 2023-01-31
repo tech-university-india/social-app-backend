f15afd32ce1ac3998dfea17bce17961ec2aef9ea
Maheshwaran K <maheshwarankirupa@gmail.com>
2023-02-01 00:39:06 +0530

    feat: add ORM integration

---
+++ database/seeders/20230131190248-demo-user.js
@@ -0,0 +1,19 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    return queryInterface.bulkInsert('Users', [{
+      userName: 'John Doe',
+      designation: 'Partner',
+      email: 'example@example.com',
+      profilePictureURL: 'https://example.com/image.jpg',
+      createdAt: new Date(),
+      updatedAt: new Date()
+    }]);
+  },
+
+  async down (queryInterface, Sequelize) {
+    return queryInterface.bulkDelete('Users', null, {});
+  }
+};
