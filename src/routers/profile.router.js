87a58a14ef5c5cff9766863f9936613584dace9f
Maheshwaran K <maheshwarankirupa@gmail.com>
2023-02-08 21:01:45 +0530

    feat: add getProfileByIdValidator

--- src/routers/profile.router.js
+++ src/routers/profile.router.js
@@ -1,7 +1,8 @@
 const profileRouter = require('express').Router();
 
 const profileController = require('../controllers/profile.controller');
+const profileValidator = require('../middlewares/profile.validator');
 
-profileRouter.get('/:userId', profileController.getUserById);
+profileRouter.get('/:userId', profileValidator.getProfileByIdValidator, profileController.getUserById);
 
 module.exports = profileRouter;
