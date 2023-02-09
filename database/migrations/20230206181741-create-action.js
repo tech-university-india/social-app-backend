c7683bb20c67ba06033c219ee3cf0059c825d66a
Maheshwaran K <maheshwarankirupa@gmail.com>
2023-02-09 22:38:25 +0530

    chore: change sequelize config to js with env var

--- database/migrations/20230206181741-create-action.js
+++ database/migrations/20230206181741-create-action.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const { actionTypes } = require('../../src/Utils/Constants');
+const { actionTypes } = require('../../src/utils/constants');
 
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
